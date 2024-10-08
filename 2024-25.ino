6d21c215a72281b92c24809bfc55450ae9a43964
kenneth01smith <kenneth01smith@gmail.com>
2024-10-08 08:46:29 -0600

    Update 2024-25.ino

--- 2024-25.ino
+++ 2024-25.ino
@@ -1,1 +1,13 @@
+//Define pin names
+//Analog pins
+#define VOLTAGE_SENSOR A0
+//PWM pins
+#define BRAKE_CONTROL 4
+#define PITCH_CONTROL 5
+//Other pins
+#define POWER_SWITCH_RELAY 28
+#define E_BUTTON 44
 
+void setup() {
+  Serial.begin(9600);  //Set baud rate to 9600 bit/s
+}
